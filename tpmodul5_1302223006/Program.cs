0bc83fe8d10806b211b7d9ca6dc910e3f7885f92
sorayahaidars <sorayahaidars14@gmail.com>
2024-03-18 02:14:48 +0700

    "menambahkan class DataGeneric"

--- tpmodul5_1302223006/Program.cs
+++ tpmodul5_1302223006/Program.cs
@@ -1,7 +1,29 @@
-﻿internal class Program
+﻿using System;
+public class DataGeneric<T>
+{
+    public T Data { get; set; }
+
+    public DataGeneric(T data)
+    {
+        Data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {Data}");
+    }
+}
+
+
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string nim = Console.ReadLine();
+        DataGeneric<string> dataGeneric = new DataGeneric<string>(nim);
+
+        dataGeneric.PrintData();
+
     }
 }
